9173fd5136553788855f38d5e9b08cb8887d5522
Jorge Olivas <olivas.daw@gmail.com>
2015-09-17 11:53:36 +0200

    a

--- ejercicio2b/ejercicio2b/server.js
+++ ejercicio2b/ejercicio2b/server.js
@@ -8,7 +8,7 @@ function inicioServer(req, res) {
     console.log('entra en inicioServer');
     var url = router(req.url);
 
-    function router(u){
+    function router(u){ //rutear URLs
         var s;
         switch (u) {
                 case '/':
@@ -30,7 +30,16 @@ function inicioServer(req, res) {
     fs.readFile(url, 'utf-8', function (err, data) {
         if (err) { console.log(err);};
         if (data) { console.log(data); }        ;
-        res.writeHead(200, { 'Content-type' : 'text/html' });
+        
+        var ext = req.url.slice(req.url.lastIndexOf('.') + 1, req.url.length);
+        switch (ext) {
+            case 'css':
+                res.writeHead(200, { 'Content-Type': 'text/css' });
+            break;
+            default:
+                res.writeHead(200, { 'Content-type' : 'text/html' });
+        }
+        
         res.write(data);
         console.log('Ha entrado en readfile');
         res.end();
---
+++ ejercicio3/ejercicio3/server.js
@@ -0,0 +1,13 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+var url = require('url');
+
+http.createServer(function (req, res) {
+    var urlParts = url.parse(req.url, true);
+    console.dir(urlParts.query);
+    var nombre = urlParts.query.name;
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hola ' + nombre + '!');
+    console.log('Petición de ' + nombre);
+}).listen(port);
