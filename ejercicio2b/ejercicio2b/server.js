290dd23f117ca666f91bab3d75088481fac59d77
Jorge Olivas <olivas.daw@gmail.com>
2015-09-17 10:53:28 +0200

    depuracion

--- ejercicio2b/ejercicio2b/server.js
+++ ejercicio2b/ejercicio2b/server.js
@@ -1,41 +1,39 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
+var fs = require('fs');
 
 http.createServer(inicioServer).listen(port);
 
 function inicioServer(req, res) {
     console.log('entra en inicioServer');
-    var url = req.url;
-    var template = router(url);
+    var url = router(req.url);
 
     function router(u){
-        var _t;
+        var s;
         switch (u) {
                 case '/':
                 case '/index':
                 case '/index.html':
-                    _t = './public/index.html';
+                    s = './public/index.html';
                     console.log('entra en swith>case: index');
                     break;
                 case '/temas':
-                    _t = './public/temas.html';
+                    s = './public/temas.html';
                     break;
                 default:
-                    _t = './public/index.html';
+                    s = './public/error.html';
                     break;       
         }
-        return _t;
+        return s;
     };
-    
-    var fs = require('fs');
-    var file = template;
 
-    fs.readFile(file, 'utf-8', function (err, data) {
-        if (err) { console.log(err); };
-        if (data){ console.log(data);};
+    fs.readFile(url, 'utf-8', function (err, data) {
+        if (err) { console.log(err);};
+        if (data) { console.log(data); }        ;
+        res.writeHead(200, { 'Content-type' : 'text/html' });
         res.write(data);
-        res.end
-        console.log('Ha entrado en readfile')
+        console.log('Ha entrado en readfile');
+        res.end();
     });
     
 };
